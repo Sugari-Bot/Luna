086f7b165263cb0405cfc3928a0d3f2b50f38601
AmazingAkai <azharmomin420@gmail.com>
2024-03-02 14:24:19 +0530

    fix setuptools.

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="Luna",
+    url="https://github.com/Sugari-Bot/Luna",
+    author="AmazingAkai",
+    author_email="akai.is.amazing@gmail.com",
+    version="2.7.0",
+    packages=[
+        "luna",
+        "luna.adapter",
+        "luna.block",
+        "luna.interface",
+    ],
+)
